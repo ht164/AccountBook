6f2a7efa3d47db77a83f25a2cd82e44a19dc4f61
ht164 <ht164.pulsar@gmail.com>
2015-01-16 00:39:15 +0900

    test file is "*.js".

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'app/scripts/*.js',
-      'test/spec/*.mocha.js'
+      'test/spec/*.js'
     ],
 
 
